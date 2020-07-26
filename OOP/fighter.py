8224098b9a9aa29360ca433cbd61452ca11da945
dan. teegan <dan.teegan@hotmail.com>
2020-07-26 18:48:05 +0100

    Python

---
+++ OOP/fighter.py
@@ -0,0 +1,18 @@
+
+class Fighter:
+    def __init__(self, fname, lname, age, wins, losses, style):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.wins = wins
+        self.losses = losses
+        self.style = style
+
+    def full_name(self):
+        return self.fname + self.lname
+
+
+
+f1 = Fighter("Jon", "Jones", 32, 20, 1, "Freestyle")
+f2 = Fighter("Conor", "Mcgregor", 30, 18, 4, "Boxer")
+f3 = Fighter("Ryan", "Hall", 29, 16, 5, "BJJ")
