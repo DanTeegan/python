05fc7c1ad466f6cc49101089882e0099d9fff55c
dan. teegan <dan.teegan@hotmail.com>
2020-07-28 19:35:20 +0100

    Calculator task

---
+++ Questions/calc.py
@@ -0,0 +1,14 @@
+
+def add (num1, num2):
+    print(num1 + num2)
+
+def sub (num1, num2):
+    return num1 - num2
+
+def times (num1, num2):
+    return num1 * num2
+
+def divide (num1, num2):
+    return num1 / num2
+
+add(5,5)
